8a28cb9d8e7d15152504b2fbedfcbec4aa5ee0ec
Aoife Sidhe <scubasidhe@gmail.com>
2023-12-08 01:18:54 +0200

    task 2 - improved logic

--- homework/aoife_sidhe/Lesson_3/task2.py
+++ homework/aoife_sidhe/Lesson_3/task2.py
@@ -1,4 +1,4 @@
-# lesson2: task2_answer = x - y / (1 + x * y)
+# Lesson2: x - y / (1 + x * y)
 # non-interactive
 import random
 
@@ -6,38 +6,43 @@ import random
 # Here we are declaring a function 'task_solution' which calculates a mathematical result
 # based on the random values of x_value and y_value
 def task_solution():
-    zero_or_not_zero = random.randint(0, 1)
-
-    if zero_or_not_zero == 0:
-        x_value = random.randint(-1000, 1000)
-        y_value = random.randint(-1000, 1000)
-    else:
-        x_value = 1
-        y_value = -1
-
-    subtraction_result = x_value - y_value
+    zero_or_not_zero, x_value, y_value = generate_numbers()
     multiplication_result = 1 + x_value * y_value
-
-    # Check if the multiplication_result is not equal to zero (to avoid division by zero error)
+    subtraction_result = x_value - y_value
+    # the error message in a variable
+    division_by_zero_error_message = "Save us! It is division by zero!"
+    # If the multiplication_result is not zero we proceed with calculations
     if multiplication_result != 0:
         calculation_result = subtraction_result / multiplication_result
-        display_answer(x_value, y_value, calculation_result)
-
-    # If the multiplication_result is zero, we print a friendly error message to the user
     else:
-        print(f"Task2\n\n({subtraction_result}) / ({multiplication_result})")  # Printing the operation
-        division_by_zero_error_message = "Save us! It is division by zero!\n"
-        print(division_by_zero_error_message)
+        # If the multiplication_result is zero, we assign the error message to calculation_result
+        calculation_result = division_by_zero_error_message
+    # Call the display_answer function with correct arguments
+    display_answer(x_value, y_value, calculation_result, zero_or_not_zero, multiplication_result, subtraction_result)
+    return multiplication_result, subtraction_result
 
 
-# This function prints the detailed result of a calculation operation
-def display_answer(x_value, y_value, calculation_result):
-    # Printing the values, using f-string for formatting
-    print(f"Task2")
-    print(f"\nX: {x_value}")
-    print(f"Y: {y_value}")
-    print(f"({x_value} - {y_value}) / (1 + {x_value} * {y_value})")  # Printing the operation
-    print(f"\nAnswer: {calculation_result}")  # Printing the result of the operation
+# This function prints the detailed result of a calculation operations, using f-string for formatting
+def display_answer(x_value, y_value, calculation_result, zero_or_not_zero, multiplication_result, subtraction_result):
+    example_l = ""
+    if zero_or_not_zero == 1:
+        example_l = "VarA"
+    if zero_or_not_zero == 0:
+        example_l = "Var B"
+    print(f"Task2 - {example_l}\nX: {x_value}\nY: {y_value}")
+    print(f"\nOperation: (x - y) / (1 + x * y)")  # Print the operation
+    print(f"It is: {subtraction_result} / {multiplication_result} ")  # Print the operation with values
+    print(f"\nAnswer: {calculation_result}")  # Print the result of the operation
+
+
+# Hewe we generate random numbers for calculations.
+def generate_numbers():
+    zero_or_not_zero = random.randint(0, 1)
+    x_value, y_value = 1, -1  # Divide by zero case
+    if zero_or_not_zero == 1:  # Random numbers for our calculations
+        x_value = random.randint(-1000, 1000)
+        y_value = random.randint(-1000, 1000)
+    return zero_or_not_zero, x_value, y_value
 
 
 # Call the function
