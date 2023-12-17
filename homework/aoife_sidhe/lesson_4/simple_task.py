61ce9589c180d8dded4fb3a87e368389a8bfb13b
Aoife Sidhe <scubasidhe@gmail.com>
2023-12-17 04:18:16 +0200

    Module simple_task - linter fix
    homework/aoife_sidhe/lesson_4/simple_task.py:34:0: R1707: Disallow trailing comma tuple (trailing-comma-tuple)
    solved
    homework/aoife_sidhe/lesson_4/simple_task.py:20:0: C0103: Constant name "last_element_tuple" doesn't conform to UPPER_CASE naming style (invalid-name)
    solved
    homework/aoife_sidhe/lesson_4/simple_task.py:49:0: C0103: Constant name "key_name" doesn't conform to UPPER_CASE naming style (invalid-name)
    solved
    homework/aoife_sidhe/lesson_4/simple_task.py:51:0: C0103: Constant name "value_type" doesn't conform to UPPER_CASE naming style (invalid-name)
    solved
    homework/aoife_sidhe/lesson_4/simple_task.py:70:0: C0206: Consider iterating with .items() (consider-using-dict-items)
    ignored
    homework/aoife_sidhe/lesson_4/simple_task.py:70:11: C0201: Consider iterating the dictionary directly instead of calling .keys() (consider-iterating-dictionary)
    solved
    homework/aoife_sidhe/lesson_4/simple_task.py:79:11: C0201: Consider iterating the dictionary directly instead of calling .keys() (consider-iterating-dictionary)
    solved

--- homework/aoife_sidhe/lesson_4/simple_task.py
+++ homework/aoife_sidhe/lesson_4/simple_task.py
@@ -1,3 +1,4 @@
+# pylint: disable=C0206
 # Lesson 4 - simple
 
 import random
@@ -17,8 +18,8 @@ my_dict = {
 print("Homework 4\n")
 
 # For 'tuple':
-last_element_tuple = my_dict['tuple'][-1]
-print("Last element in 'tuple':", last_element_tuple)
+LAST_ELEMENT_TUPLE = my_dict['tuple'][-1]
+print("Last element in 'tuple':", LAST_ELEMENT_TUPLE)
 
 # For 'list':
 print(f"\nList was {my_dict['list']}")
@@ -31,32 +32,32 @@ print("\n****************************")
 print("\nDictionary\n")
 random_values = tuple(random.randint(0, 100) for _ in range(6))
 # my_dict[('i am a tuple!',)] = 100
-values_for_tuple = 100,
+values_for_tuple = (100,)
 # my_dict[('i am a tuple!',)] = random_values
 my_dict['dict'][('i am a tuple!',)] = values_for_tuple
 
 # Removing an arbitrary element from the dictionary, let's remove 'key3'
 del my_dict['dict']['key3']
 
-key_name = ('i am a tuple!',)
+KEY_NAME = ('i am a tuple!',)
 # Print the key-value pair
-print(f"{key_name}: {my_dict['dict'][key_name]}")
-if isinstance(my_dict['dict'][key_name], tuple):
-    print(f"{key_name}: is a tuple.\n")
+print(f"{KEY_NAME}: {my_dict['dict'][KEY_NAME]}")
+if isinstance(my_dict['dict'][KEY_NAME], tuple):
+    print(f"{KEY_NAME}: is a tuple.\n")
 else:
-    print(f"{key_name}: is not a tuple.\n")
+    print(f"{KEY_NAME}: is not a tuple.\n")
 
-key_name = "('i am a tuple',)"
-my_dict['dict'][key_name] = 100  # add a new key-value pair
-value_type = str(type(my_dict['dict'][key_name]))[8:-2]
-print(f"{key_name}: {my_dict['dict'][key_name]}")
+KEY_NAME = "('i am a tuple',)"
+my_dict['dict'][KEY_NAME] = 100  # add a new key-value pair
+VALUE_TYPE = str(type(my_dict['dict'][KEY_NAME]))[8:-2]
+print(f"{KEY_NAME}: {my_dict['dict'][KEY_NAME]}")
 
-if isinstance(my_dict['dict'][key_name], tuple):
-    print(f"{key_name} : is a tuple.\n")
+if isinstance(my_dict['dict'][KEY_NAME], tuple):
+    print(f"{KEY_NAME} : is a tuple.\n")
 else:
-    print(f"{key_name} : is not a tuple. It is of type {value_type}.")
+    print(f"{KEY_NAME} : is not a tuple. It is of type {VALUE_TYPE}.")
     print("Deleting...")
-    del my_dict['dict'][key_name]
+    del my_dict['dict'][KEY_NAME]
 
 # For 'set':
 print("\n****************************\n")
@@ -66,25 +67,30 @@ my_dict['my_set'].pop()  # Removing an arbitrary element from the set
 print(f"My set is {my_dict['my_set']}\n")
 
 # Print all keys in my_dict
-print("Print all keys in my_dict...")
-for key in my_dict.keys():
-    print(key)
+print("\n****************************\n")
+print("Print all keys in my_dict...\n")
+for key, value in my_dict.items():
+    print(f'{key}: {value}')
     if isinstance(my_dict[key], dict):
         print("\t... and its keys:")
-        for sub_key in my_dict[key]:
-            print("\t", sub_key)
+        for sub_key, sub_value in my_dict[key].items():
+            print(f'{sub_key}: {sub_value}')
 
 # We can also check if the key is a tuple, and if so, print its elements:
 print("\nIf key is a tuple, print its elements...")
-for key in my_dict.keys():
+for key, value in my_dict['dict'].items():
     if isinstance(key, tuple):
         print(f"Key '{key}' is a tuple with elements:")
         for i, tuple_element in enumerate(key):
             print(f"  Element {i}: {tuple_element}")
+        print(f"Value associated with tuple key '{key}': {value}")
+
+print("\n****************************\n")
+
 
 # Print the value of a particular key in my_dict
-particular_key = ('i am a tuple!',)
-print(my_dict['dict'][particular_key])
+# particular_key = ('i am a tuple!',)
+# print(my_dict['dict'][particular_key])
 
 # Displaying the updated dictionary
 print("\nFinal Result:")
